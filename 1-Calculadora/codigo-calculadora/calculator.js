8b0a7da054dab192557b784d2794ec8c0d92edd3
Felipe <felipe.machado.silva.05@gmail.com>
2019-07-17 09:38:20 -0300

    Date e Hora
    Trabalhamos com data e hora,utilizando o metodo setInterval para atualizar a hora a cada 1 segundo ,além de formata-lá  em pt-BR

--- 1-Calculadora/codigo-calculadora/calculator.js
+++ 1-Calculadora/codigo-calculadora/calculator.js
@@ -1,11 +1,1 @@
-/* MVC
-
-View: interação usuário
-Controller: Regras de negocio
-Model: Acessar os dados
-
-
-*/
-
-// instancia um objeto calculadora seguindo o molde da classe CalcController
-let calculator = new CalcController;
+window.calculator = new CalcController();
--- 1-Calculadora/codigo-calculadora/controller/calcController.js
+++ 1-Calculadora/codigo-calculadora/controller/calcController.js
@@ -9,52 +9,80 @@
 
 
 */
-class CalcController{
-    //Construtor : metodo para construir um objeto,utilizado para colocar variáveis e etc
-    constructor(){
-        //this é dizer este objeto,sendo uma maneira em js para definir que é um atributo e metodos
+class CalcController {
 
-        // formas de encapsulamento private,public e protected no js ainda não EXISTE para representar esta ideia utilizamos uma convenção _inicioAtributo
+    constructor() {
 
-        this._displayCalc = "0";
+        this._locale = "pt-BR"
         this._currentDate;
+        //Colocamos os nossos elementos na DOM como atributos 
+        this._displayCalcEl = document.querySelector("#display");
+        this._timeEl = document.querySelector("#hora");
+        this._dateEl = document.querySelector("#data");
+
         this.initialize();
     }
 
-    initialize(){
-        //DOM é a manipulação da arvore de elementos do html ou seja colocar uma nova div e muito mais
-        // usamos os eventos como gatilhos pra fazer alterações no DOM
+    initialize() {
+        this.setDisplayDateTime();
 
-        //utilizamos o comando document.querySelector para pegar por seletor um campo de nosso html ,amarrando ele podemos fazer alterações
-        //usamos a convenção el para indicar que é um elemento do nosso document
-        
-let displayCalcEl= document.querySelector("#display");
-let timeEl = document.querySelector("#hora");
-let dataEl = document.querySelector("#data");
-      //coloque uma informação neste elemento no formato html
-      displayCalcEl.innerHTML = "4999"
-      timeEl.innerHTML = "10:50"
-      dataEl.innerHTML = "17/07/2019"
-       
+        //setInterval utilizado para atualizar a cada 1000 ms no caso 1 segundo...
+        setInterval(() => {
+            this.setDisplayDateTime();
+        }, 1000)
+
     }
 
-    //Todo atributo queremos atribuir ou mostrar um dado ou seja utilizamos o get e set
-
-//retornamos o dado
-    get displayCalc(){
-        return this._displayCalc;
-    }
-//atribuimos um valor
-    set displayCalc(valor){
-        this._displayCalc = valor;
-    }
-
-    get dataAtual(){
-        return this._currentDate
+    //metodo usado para retornar data formatada utilizando o metodo toLocalDate/TimeString
+    setDisplayDateTime() {
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+            //formatando as datas com os seguintes metodos
+            //Incluir anotações na documentação https://docs.w3cub.com/javascript/global_objects/date/tolocaledatestring/
+            day:"2-digit",
+            month:"long",
+            year:"numeric"
+        });
     }
 
 
-    set dataAtual(valor){
+    get displayTime() {
+        return this._timeEl.innerHTML;
+    }
+
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+
+
+    set displayTime(value) {
+        this._timeEl.innerHTML = value;
+    }
+
+    set displayDate(value) {
+        this._dateEl.innerHTML = value;
+    }
+
+
+
+    // usamos o innerHTML para modificar e mostrar o valor de nosso elemento
+
+
+    get displayCalc() {
+        return this._displayCalcEl.innerHTML;
+    }
+
+    set displayCalc(value) {
+        this._displayCalcEl.innerHTML = value;
+    }
+
+    get currentDate() {
+        //Classe do js responsavel pela parte de datas.
+        return new Date();
+    }
+
+
+    set currentDate(valor) {
         this._currentDate = valor;
     }
 }
