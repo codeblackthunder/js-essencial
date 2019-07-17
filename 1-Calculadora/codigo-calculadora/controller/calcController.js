60d70491599e9d0b618aa7cf1008d7bd820c3424
Felipe <felipe.machado.silva.05@gmail.com>
2019-07-17 06:59:16 -0300

    Encapsulamento  e construtor
    Fizemos o getter e setter de um atributo,foi explicado a convenção _

--- 1-Calculadora/codigo-calculadora/controller/calcController.js
+++ 1-Calculadora/codigo-calculadora/controller/calcController.js
@@ -4,11 +4,19 @@
     função dentro de uma classe se chama metodo
 
     então basicamente uma classe é um conjunto de metodos e atributos
-    
+
 
 
 
 */
 class CalcController{
+    //Construtor : metodo para construir um objeto,utilizado para colocar variáveis e etc
+    constructor(){
+        //this é dizer este objeto,sendo uma maneira em js para definir que é um atributo e metodos
 
+        // formas de encapsulamento private,public e protected no js ainda não EXISTE para representar esta ideia utilizamos uma convenção _inicioAtributo
+
+        this._displayCalc = "0";
+        this._dataAtual;
+    }
 }
